829cd5ca06ab70fee65dfb56a065a8bde0087094
Luke Bond <luke.n.bond@gmail.com>
2015-05-14 21:14:01 +0100

    Added substitution for scheduler URL on Docker build.
    Fixes #31

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Connecting to orchestrator at $PAZ_ORCHESTRATOR_URL & $PAZ_ORCHESTRATOR_SOCKET"
 perl -pi -e "s/localhost:9000/$PAZ_ORCHESTRATOR_URL/g" assets/paz-ember-*.js
+perl -pi -e "s/localhost:9002/$PAZ_SCHEDULER_URL/g" assets/paz-ember-*.js
 perl -pi -e "s/localhost:1337/$PAZ_ORCHESTRATOR_SOCKET:80/g" assets/paz-ember-*.js
 perl -pi -e "s/localhost%3A9000/$PAZ_ORCHESTRATOR_URL/g" index.html
 perl -pi -e "s/localhost%3A1337/$PAZ_ORCHESTRATOR_SOCKET%3A80/g" index.html
