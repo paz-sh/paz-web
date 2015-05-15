593a745b188a6ea2e84031eed524f54fd8610cbc
Luke Bond <luke.n.bond@gmail.com>
2015-05-15 12:15:17 +0100

    Added second substitution for scheduler URL on Docker build.
    Actually fixes #31

--- bin/run.sh
+++ bin/run.sh
@@ -4,5 +4,6 @@ perl -pi -e "s/localhost:9000/$PAZ_ORCHESTRATOR_URL/g" assets/paz-ember-*.js
 perl -pi -e "s/localhost:9002/$PAZ_SCHEDULER_URL/g" assets/paz-ember-*.js
 perl -pi -e "s/localhost:1337/$PAZ_ORCHESTRATOR_SOCKET:80/g" assets/paz-ember-*.js
 perl -pi -e "s/localhost%3A9000/$PAZ_ORCHESTRATOR_URL/g" index.html
+perl -pi -e "s/localhost%3A9002/$PAZ_SCHEDULER_URL/g" index.html
 perl -pi -e "s/localhost%3A1337/$PAZ_ORCHESTRATOR_SOCKET%3A80/g" index.html
 harp server -p 80 .
